980a7faf67417b5814922f662db648d8210b9116
Admer <49600504+Admer456@users.noreply.github.com>
2021-09-24 21:56:16 +0200

    Update common/Precompiled.hpp with a lot of stuff

--- common/Precompiled.hpp
+++ common/Precompiled.hpp
@@ -1,11 +1,25 @@
 
-// C++ STL includes
-#include <array>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <filesystem>
-#include <chrono>
-#include <type_traits>
+#pragma once
+
+// adm-utils has all the precompiled stuff we need :)
+#include "extern/adm-utils/src/Precompiled.hpp"
+
+// Some usings & namespaces to shorten typing
+namespace fs = std::filesystem;
+namespace chrono = std::chrono;
+using Path = fs::path;
+using String = std::string;
+using StringRef = std::string_view;
+using File = std::fstream;
+
+inline const String NullString = "";
+
+// Include our interfaces tho'
+#include "common/interfaces/ICommon.hpp"
+#include "common/interfaces/IEngine.hpp"
+#include "common/interfaces/IFileSystem.hpp"
+#include "common/interfaces/IGame.hpp"
 
 
+// NO ENGINE-SPECIFIC HEADERS
+// Cuz' this is used by the game library as well
