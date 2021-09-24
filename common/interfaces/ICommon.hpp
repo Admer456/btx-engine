bf3556c0dd96b66ace52b66cdc3fb816a6bb5f21
Admer <49600504+Admer456@users.noreply.github.com>
2021-09-24 21:54:42 +0200

    ICommon: add Init, update formatting

--- common/interfaces/ICommon.hpp
+++ common/interfaces/ICommon.hpp
@@ -3,16 +3,17 @@
 class ICommon
 {
 public:
+    virtual void    Init() = 0;
+
     // Use adm::format() if you wanna format it
-    virtual void Printf( const char* string ) = 0;
-    virtual void DPrintf( const char* string, int developerLevel ) = 0;
-    virtual void Warning( const char* string ) = 0;
-    virtual void Error( const char* string ) = 0;
+    virtual void    Print( const char* string ) = 0;
+    virtual void    DPrint( const char* string, int developerLevel ) = 0;
+    virtual void    Warning( const char* string ) = 0;
+    virtual void    Error( const char* string ) = 0;
 
     // Returns the current time since the app started running
-    virtual float Time() const = 0;
+    virtual float   Time() const = 0;
 
     // Is this a dedicated server?
-    virtual bool IsDedicatedServer() const = 0;
-
+    virtual bool    IsDedicatedServer() const = 0;
 };
