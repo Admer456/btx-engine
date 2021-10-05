b5369e613c17a1010e1d60a1315546549365cda3
Admer <49600504+Admer456@users.noreply.github.com>
2021-10-05 21:34:47 +0200

    Core: fancy comments

--- engine/core/Core.cpp
+++ engine/core/Core.cpp
@@ -5,36 +5,59 @@
 
 CVar developer( "developer", "0", 0, "Developer mode" );
 
+// ============================
+// Core::Init
+// ============================
 void Core::Init()
 {
 	systemTimer.Reset();
 }
 
+// ============================
+// Core::Shutdown
+// There's nothing to do here at the moment,
+// so shutting down the core would be the safest thing in this codebase
+// ============================
 void Core::Shutdown()
 {
 
 }
 
+// ============================
+// Core::DevLevel
+// ============================
 int Core::DevLevel() const
 {
 	return developer.GetInt();
 }
 
+// ============================
+// Core::Time
+// ============================
 float Core::Time() const
-{	
+{
 	return systemTimer.GetElapsed( adm::Timer::Seconds );
 }
 
+// ============================
+// Core::TimeMilliseconds
+// ============================
 float Core::TimeMilliseconds() const
 {
 	return systemTimer.GetElapsed( adm::Timer::Milliseconds );
 }
 
+// ============================
+// Core::IsDedicatedServer
+// ============================
 bool Core::IsDedicatedServer() const
 {	// No dedicated server code yet
 	return false;
 }
 
+// ============================
+// Core::IsHost
+// ============================
 bool Core::IsHost() const
 {	// For now, we only got singleplayer
 	return true;
