c418dd7b4a690cc2c14f50fc5255d11b9d025680
Admer <49600504+Admer456@users.noreply.github.com>
2021-10-05 20:44:41 +0200

    Input: use ICore and IConsole

--- engine/input/Input.cpp
+++ engine/input/Input.cpp
@@ -13,12 +13,16 @@ void Input::Init()
 {
 	detail::gInput = this;
 
+	console->Print( "Input::Init" );
+
 	keys.clear();
 	axes.clear();
 }
 
 void Input::Shutdown()
 {
+	console->Print( "Input::Shutdown" );
+
 	keys.clear();
 	axes.clear();
 }
@@ -58,11 +62,12 @@ void Input::RegisterKey( InputKey* key )
 	{
 		if ( localKey->GetName() == key->GetName() )
 		{
-			//console->Warning
+			console->Warning( adm::format( "Input::RegisterKey: Can't register existing key '%s'", key->GetName().data() ) );
 			return;
 		}
 	}
 
+	console->Print( adm::format( "Input::RegisterKey: Registered '%s'", key->GetName().data() ) );
 	keys.push_back( key );
 }
 
@@ -72,7 +77,7 @@ void Input::RegisterAxis( InputAxis* axis )
 	{
 		if ( localAxis->GetName() == axis->GetName() )
 		{
-			//console->Warning
+			console->Warning( adm::format( "Input::RegisterAxis: Can't register existing axis '%s'", axis->GetName().data() ) );
 			return;
 		}
 	}
--- engine/input/Input.hpp
+++ engine/input/Input.hpp
@@ -12,7 +12,16 @@ public:
     void RegisterKey( InputKey* key ) override;
     void RegisterAxis( InputAxis* axis ) override;
 
+    void Setup( ICore* core, IConsole* console )
+    {
+        this->core = core;
+        this->console = console;
+    }
+
 private:
     std::vector<InputKey*> keys;
     std::vector<InputAxis*> axes;
+
+    ICore* core{ nullptr };
+    IConsole* console{ nullptr };
 };
