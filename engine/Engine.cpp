dd4471e973c33dfa5865513bdc93f10fae2a20ee
Admer <49600504+Admer456@users.noreply.github.com>
2021-10-05 21:34:59 +0200

    Engine: fancy comments

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -9,6 +9,11 @@
 #include <iostream>
 #include "Engine.hpp"
 
+// ============================
+// BTXMain
+// extern-declared elsewhere so we don't have
+// to include engine headers there
+// ============================
 int BTXMain( int argc, char** argv )
 {
 	return Engine::Main( argc, argv );
@@ -22,6 +27,10 @@ CVar testVar4( "engineTestVar4", "20.49", 0, "This is a test variable" );
 InputKey inputType( "start_typing", SDL_SCANCODE_RETURN );
 InputKey inputExit( "quit", SDL_SCANCODE_ESCAPE );
 
+// ============================
+// Engine::Main
+// The place where it all started...
+// ============================
 int Engine::Main( int argc, char** argv )
 {
 	Engine& engine = adm::Singleton<Engine>::GetInstance();
@@ -37,6 +46,9 @@ int Engine::Main( int argc, char** argv )
 	return 0;
 }
 
+// ============================
+// Engine::Init
+// ============================
 void Engine::Init( int argc, char** argv )
 {
 	SDL_Init( SDL_INIT_VIDEO | SDL_INIT_EVENTS );
@@ -57,7 +69,8 @@ void Engine::Init( int argc, char** argv )
 	InputKey::RegisterAll();
 	InputAxis::RegisterAll();
 
-	// Initialise the filesystem with the "base" folder
+	// Initialise the filesystem with the directory of the
+	// executable's name and the "base" directory
 	Path currentExe = argv[0];
 	fileSystem.Setup( &core, &console );
 	fileSystem.Init( currentExe.filename().stem() );
@@ -69,9 +82,14 @@ void Engine::Init( int argc, char** argv )
 	console.Print( adm::format( "Developer level: %i", core.DevLevel() ) );
 }
 
+// ============================
+// Engine::Shutdown
+// Shuts down all subsystems in order of dependency
+// ============================
 void Engine::Shutdown()
 {
 	console.Print( "Shutting down..." );
+
 	input.Shutdown();
 	fileSystem.Shutdown();
 	console.Shutdown();
@@ -80,11 +98,17 @@ void Engine::Shutdown()
 	SDL_Quit();
 }
 
+// ============================
+// Engine::RunFrame
+// ============================
 bool Engine::RunFrame()
 {
-	std::this_thread::sleep_for( chrono::milliseconds( 16U ) );
+	std::this_thread::sleep_for( chrono::milliseconds( 16U ) ); // temporary
+	
+	// Update the keyboard state etc.
 	input.Update();
 
+	// tmporary
 	if ( inputType.IsPressed() )
 	{
 		console.Print( "Enter your command:" );
@@ -103,6 +127,9 @@ bool Engine::RunFrame()
 	return !inputExit.IsPressed();
 }
 
+// ============================
+// Engine::Command_Mount
+// ============================
 bool Engine::Command_Mount( StringRef args )
 {
 	Engine& self = adm::Singleton<Engine>::GetInstance();
