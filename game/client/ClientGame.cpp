1c260809e342d3cc6fd62d978d76cc6539301646
admer456-stu <admer.suko@edu.fit.ba>
2022-04-11 09:10:47 +0200

    Game: testing CVars and inputs

--- game/client/ClientGame.cpp
+++ game/client/ClientGame.cpp
@@ -2,6 +2,10 @@
 #include "Precompiled.hpp"
 #include "ClientGame.hpp"
 
+// Absolute mouse position inside the window
+InputAxis iMouseX( AxisCodes::MouseX );
+InputAxis iMouseY( AxisCodes::MouseY );
+
 bool ClientGame::Init()
 {
 	Console->Print( "ClientGame::Init" );
@@ -13,7 +17,15 @@ void ClientGame::Shutdown()
 	Console->Print( "ClientGame::Shutdown" );
 }
 
-void ClientGame::Update( const float& deltaTime )
+void ClientGame::Update()
 {
+	static float timer = 0.0f;
 
+	if ( timer > 0.5f )
+	{
+		timer = 0.0f;
+		Console->Print( adm::format( "x: %3.2f, y: %3.2f", iMouseX.GetValue(), iMouseY.GetValue() ) );
+	}
+
+	timer += Core->DeltaTime();
 }
