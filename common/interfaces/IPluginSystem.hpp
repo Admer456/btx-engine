a06b9e160ee24df80e41b6e01b16d88f96bb6aa0
Admer <49600504+Admer456@users.noreply.github.com>
2022-07-19 20:50:27 +0200

    IPluginSystem: simplify a bit, add PluginLibraryHandle, add GetPluginList

--- common/interfaces/IPluginSystem.hpp
+++ common/interfaces/IPluginSystem.hpp
@@ -19,8 +19,9 @@ struct PluginLibraryMetadata
 	Vector<PluginMetadata> pluginInfo;
 };
 
-template<typename pluginType>
-using PluginList = LinkedList<pluginType>;
+// I hope to God you won't actually use ~4 billion plugins
+using PluginLibraryHandle = uint32_t;
+constexpr PluginLibraryHandle PluginLibraryInvalid = ~0U;
 
 class IPluginSystem
 {
@@ -30,21 +31,24 @@ public:
 
 	// Reads plugin metadata and loads a plugin DLL
 	// One plugin library may load multiple plugins
-	virtual int LoadPluginLibrary( Path libraryPath ) = 0;
+	virtual PluginLibraryHandle LoadPluginLibrary( Path libraryPath ) = 0;
 
 	// Gets plugin library info
-	virtual const PluginLibraryMetadata& GetPluginLibraryMetadata( const int& libraryHandle ) const = 0;
+	virtual const PluginLibraryMetadata& GetPluginLibraryMetadata( PluginLibraryHandle libraryHandle ) const = 0;
 	virtual const PluginLibraryMetadata& GetPluginLibraryMetadata( const IPlugin* plugin ) const = 0;
 
 	// Shuts down the plugin and unloads the DLL
-	virtual void UnloadPluginLibrary( const int& libraryHandle ) = 0;
+	virtual void UnloadPluginLibrary( PluginLibraryHandle libraryHandle ) = 0;
 
+	// Gets a list of plugins that implement interfaceName
+	virtual const LinkedList<IPlugin*>& GetPluginList( const char* interfaceName ) const = 0;
+	
 	// Gets a plugin instance by name
 	template<typename pluginType>
-	pluginType* GetPluginByName( StringView pluginName, int minimumVersion ) const
+	pluginType* GetPluginByName( StringView pluginName ) const
 	{
-		return dynamic_cast<pluginType*>( GetPluginByNameRaw( pluginName, minimumVersion ) );
+		return dynamic_cast<pluginType*>( GetPluginByNameRaw( pluginName ) );
 	}
 
-	virtual IPlugin* GetPluginByNameRaw( StringView pluginName, int minimumVersion ) const = 0;
+	virtual IPlugin* GetPluginByNameRaw( StringView pluginName ) const = 0;
 };
